95cf171c80c1655748e3ad04ec2c9ed690852c79
Tahiryar <tahiryar.251k@gmail.com>
2025-04-08 14:12:15 +0500

    create function of user voice record

--- brain_of_docter.py
+++ brain_of_docter.py
@@ -1,20 +1,44 @@
 import os
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-//convert img to binary
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
 
+# Step2: Convert image to required format
 import base64
 
-img_path='img.png'
+# image_path = "acne.jpg"
 
-img_file=open(img_path,'rb')
+def encode_image(image_path):
+    image_file = open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
 
-encoded_img=base64.b64encode(img_file.read()).decode('utf-8')
-
-//setup multimodal LLM
-
+# Step3: Setup Multimodal LLM
 from groq import Groq
 
-client=Groq()
-model="llama-3.2-90b-vision-preview"
+query = "Is there something wrong with my face?"
+model = "llama-3.2-90b-vision-preview"
 
+def analyze_image_with_query(query, model, encoded_image):
+    client = Groq(api_key=GROQ_API_KEY)
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": query
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{encoded_image}",
+                    },
+                },
+            ],
+        }
+    ]
+    chat_completion = client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
+
+    return chat_completion.choices[0].message.content
