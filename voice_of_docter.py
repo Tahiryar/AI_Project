2410c89ff51f88baf98c07871adfda3cb78ba336
Tahiryar <tahiryar.251k@gmail.com>
2025-04-10 22:24:58 +0500

    create function using gtts to speech voice

---
+++ voice_of_docter.py
@@ -0,0 +1,17 @@
+#Step1a: Setup Text to Speech–TTS–model with gTTS
+import os
+from gtts import gTTS
+
+def text_to_speech_with_gtts_old(input_text, output_filepath):
+    language="en"
+
+    audioobj= gTTS(
+        text=input_text,
+        lang=language,
+        slow=False
+    )
+    audioobj.save(output_filepath)
+
+
+input_text="Hi this is Ai with Hassan!"
+text_to_speech_with_gtts_old(input_text=input_text, output_filepath="gtts_testing.mp3")
