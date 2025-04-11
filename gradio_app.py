ad18555468ddcc4125db7035ce342f817da3f397
Tahiryar <tahiryar.251k@gmail.com>
2025-04-11 12:21:45 +0500

    create process input function

--- gradio_app.py
+++ gradio_app.py
@@ -7,6 +7,23 @@ from voice_of_the_patient import record_audio, transcribe_with_groq
 from voice_of_the_doctor import text_to_speech_with_gtts, 
 
 
+def process_inputs(audio_filepath, image_filepath):
+    speech_to_text_output = transcribe_with_groq(GROQ_API_KEY=os.environ.get("GROQ_API_KEY"), 
+                                                 audio_filepath=audio_filepath,
+                                                 stt_model="whisper-large-v3")
+
+    # Handle the image input
+    if image_filepath:
+        doctor_response = analyze_image_with_query(query=system_prompt+speech_to_text_output, encoded_image=encode_image(image_filepath), model="llama-3.2-11b-vision-preview")
+    else:
+        doctor_response = "No image provided for me to analyze"
+
+    voice_of_doctor = text_to_speech_with_elevenlabs(input_text=doctor_response, output_filepath="final.mp3") 
+
+    return speech_to_text_output, doctor_response, voice_of_doctor
+
+
+
 
 # Create the interface
 iface = gr.Interface(
