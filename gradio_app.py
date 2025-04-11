946ed96cd8d0f0c317a60fb919dc084efb8f1eca
Tahiryar <tahiryar.251k@gmail.com>
2025-04-11 12:18:17 +0500

    create interface of gradio voice

---
+++ gradio_app.py
@@ -0,0 +1,26 @@
+#VoiceBot UI with Gradio
+import os
+import gradio as gr
+
+from brain_of_the_doctor import encode_image, analyze_image_with_query
+from voice_of_the_patient import record_audio, transcribe_with_groq
+from voice_of_the_doctor import text_to_speech_with_gtts, 
+
+
+
+# Create the interface
+iface = gr.Interface(
+    fn=process_inputs,
+    inputs=[
+        gr.Audio(sources=["microphone"], type="filepath"),
+        gr.Image(type="filepath")
+    ],
+    outputs=[
+        gr.Textbox(label="Speech to Text"),
+        gr.Textbox(label="Doctor's Response"),
+        gr.Audio("Temp.mp3")
+    ],
+    title="AI Doctor with Vision and Voice"
+)
+
+iface.launch(debug=True)
